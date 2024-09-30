3c8bc59596219f85faa0eb8650fe2f2280c9e731
111260 <muhammadafshalsajid@gmail.com>
2024-09-30 16:33:24 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,9 @@
-def call(String Project, String ImageTag, String dockerhubuser){
+def call(String Project, String ImageTag, String dockerhubuser) {
   withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'DOCKERHUB_PASS', usernameVariable: 'DOCKERHUB_USER')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}}"
+      // Here, use the secured environment variables provided by withCredentials
+      sh "docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}"
   }
+  // Pushing the image using the function's dockerhubuser parameter
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+}
 }
