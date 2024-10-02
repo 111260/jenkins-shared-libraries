c6710eb41914c1a22214f08c417fb17d729bde0c
111260 <muhammadafshalsajid@gmail.com>
2024-10-03 03:53:01 +0500

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,7 +1,7 @@
 def call() {
   withEnv([
     'TRIVY_AUTH_URL=https://ghcr.io',
-    'TRIVY_TOKEN=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' // Replace <your_github_token> with your actual token or use a Jenkins secret variable
+    'TRIVY_TOKEN=' // Replace <your_github_token> with your actual token or use a Jenkins secret variable
   ]) {
     try {
       // Run Trivy scan
