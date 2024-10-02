e391e0903921b005ed62d013c416a90f04efd4b4
111260 <muhammadafshalsajid@gmail.com>
2024-10-03 01:37:45 +0500

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,7 @@
 def call(){
-  timeout(time: 20, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
+  timeout(time: 20, unit: "MINUTES") {
+      echo "Waiting for SonarQube quality gate..."
+      waitForQualityGate abortPipeline: true
+      echo "Quality gate check completed."
   }
 }
