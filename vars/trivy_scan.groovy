2e8558809f38ab0d862f8e846c2f4dbf48c38f20
111260 <muhammadafshalsajid@gmail.com>
2024-10-02 17:19:59 +0500

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,15 @@
-def call(){
-  sh "trivy fs --skip-update ."
+def call() {
+  withEnv([
+    'TRIVY_AUTH_URL=https://ghcr.io',
+    'TRIVY_TOKEN=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' // Replace <your_github_token> with your actual token or use a Jenkins secret variable
+  ]) {
+    try {
+      // Run Trivy scan
+      sh "trivy fs ."
+    } catch (Exception e) {
+      // Handle any errors that occur during the Trivy scan
+      echo "Error occurred while running Trivy scan: ${e}"
+    }
+  }
 }
+
